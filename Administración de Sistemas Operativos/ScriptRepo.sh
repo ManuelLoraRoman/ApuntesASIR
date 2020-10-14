5ed647e56c1b3fb9274085179e00fcfedfa837b9
Manuel Lora Román <manuelloraroman@gmail.com>
2020-10-14 10:07:13 +0200

    He añadido contenido nuevo

---
+++ Administración de Sistemas Operativos/ScriptRepo.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Realiza un script que introduciéndolo como parámetro el nombre de un
+# repositorio, muestre como salida los paquetes de ese repositorio que
+# están instalados en la máquina.
+
+echo "\n"
+echo "SELECCIÓN DE PAQUETES POR REPOSITORIO"
+echo "---------------------------------------"
+
+echo "Repositorio introducido: " $1
+
+if echo "$1" | grep "^http\:\/\/.\.*" > /dev/null
+then
+	echo "\nPrimera opción seleccionada\n"
+
+elif echo "$1" | grep ".\.*\.*" > /dev/null
+then
+	echo "\nSegunda opción seleccionada\n"
+
+else
+	echo "\nError"
+fi
+
+for p in $(dpkg -l | grep '^ii' | cut -d ' ' -f 3);
+
+# Esta primera sección del for permite visualizar el nombre de los paquetes
+# instalados en nuestra máquina e iterar sobre ellos.
+
+	do apt-cache showpkg $p | head -3 | grep -v '^Versions' | sed -e 's/Package: //;' | paste - - | grep $1 | awk -F '\t' '{print $1}';
+done;
+
+# security.debian.org
+# http://security.debian.org
