d5269c489e3400897dd2c2e69f62466d8a9efad3
Manuel Lora Román <manuelloraroman@gmail.com>
2020-10-14 13:49:17 +0200

    He borrado cositas

--- Administración de Sistemas Operativos/ScriptRepo.sh
+++ Administración de Sistemas Operativos/ScriptRepo.sh
@@ -18,23 +18,3 @@ if echo "$1" | grep "^http\:\/\/.\.*" > /dev/null
 then
 
 	VAR=`echo "$1" | cut -f3 -d"/"`
-
-
-elif echo "$1" | grep ".\.*\.*" > /dev/null
-then
-	echo "\nSegunda opción seleccionada\n"
-
-else
-	echo "\nError"
-fi
-
-for p in $(dpkg -l | grep '^ii' | cut -d ' ' -f 3);
-
-# Esta primera sección del for permite visualizar el nombre de los paquetes
-# instalados en nuestra máquina e iterar sobre ellos.
-
-	do apt-cache showpkg $p | head -3 | grep -v '^Versions' | sed -e 's/Package: //;' | paste - - | grep $VAR | awk -F '\t' '{print $1}';
-done;
-
-# security.debian.org
-# http://security.debian.org
