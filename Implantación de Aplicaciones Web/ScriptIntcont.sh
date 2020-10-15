7af47de1fd89d982158e902a7ac48357aec29117
Manuel Lora Román <manuelloraroman@gmail.com>
2020-10-15 19:47:56 +0200

    He añadido contenido nuevo

--- Implantación de Aplicaciones Web/ScriptIntcont.sh
+++ Implantación de Aplicaciones Web/ScriptIntcont.sh
@@ -1,2 +1,27 @@
 #! /bin/bash
 
+echo -e "Bienvenid@ a la integración continua con Docusaurus y surge.\n"
+
+echo -e "En primer lugar, introduzca el nombre de un directorio para crear hay su página:"
+
+read directorio
+
+mkdir $directorio
+
+cd $directorio
+
+echo -e "Introduzca a continuación el nombre de su página:"
+
+read pagina
+
+npx @docusaurus/init@next init $pagina classic
+
+cd $pagina
+
+npm run build
+
+cd build
+
+echo "$pagina.surge.sh" > CNAME
+
+surge
